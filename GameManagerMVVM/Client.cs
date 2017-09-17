c8f48ce1afe66c570b9a7c8cd2ee31c23af066e0
Schmitt <clement.flance@gmail.com>
2017-09-18 01:23:19 +0200

    Initial commit, add WPF interface

---
+++ GameManagerMVVM/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremiereUtilisationDataContext
+{
+    public class Client
+    {
+        private string nom;
+        private string prenom;
+        private string sexe;
+        private int age;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Sexe { get => sexe; set => sexe = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
